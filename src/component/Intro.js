32f335a6c9aa2c7adb893685a51445581e51f3ec
unknown <raffyamoguis@gmail.com>
2022-04-02 10:55:07 +0800

    fix spacing

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -6,22 +6,22 @@ const Intro = () => {
             <div className="row justify-content-center">
                 <div className="col-md mt-5">
                     <h1><i className="bi bi-tv ico"></i></h1>
-                    <h3 className='f-b'>Front-end</h3>
-                    <h5>I value simple content structure, and thoughtful interactions.</h5>
-                    <h5 className="color-secondary">Front-end languages I know:</h5>
+                    <h3 className='mt-3'><b />Front-end</h3>
+                    <h5 className='mt-2'>I value simple content structure, and thoughtful interactions.</h5>
+                    <h5 className="color-secondary mt-4">Front-end languages I know:</h5>
                     <p>HTML, CSS, Bootstrap</p>
                     <p>Javascript, Jquery, React.js</p>
-                    <h5 className="color-secondary">Design Tools</h5>
+                    <h5 className="color-secondary mt-4">Design Tools</h5>
                     <p>Framer</p>
                     <p>Adobe Photoshop</p>
                 </div>
                 <div className="col-md mt-5">
                     <h1><i className="bi bi-code-slash ico"></i></h1>
-                    <h3 className='f-b'>Back-end</h3>
-                    <h5>I like to code things from scratch, and enjoy bringing ideas to life.</h5>
-                    <h5 className="color-secondary">Back-end languages I know:</h5>
+                    <h3 className='mt-3'> <b />Back-end</h3>
+                    <h5 className='mt-2'>I like to code things from scratch, and enjoy bringing ideas to life.</h5>
+                    <h5 className="color-secondary mt-4">Back-end languages I know:</h5>
                     <p>PHP, Laravel, Python, Node.js, Java</p>
-                    <h5 className="color-secondary">Coding Tools</h5>
+                    <h5 className="color-secondary mt-4">Coding Tools</h5>
                     <p>VS Code</p>
                     <p>Sublime Text</p>
                     <p>Github</p>
--- src/component/OngoingProjects.js
+++ src/component/OngoingProjects.js
@@ -10,7 +10,7 @@ const OngoingProjects = () => {
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6 col-lg-4">
-                            <div class="card-box text-center p-5">
+                            <div class="card-box text-center p-5 border shadow">
                                 <img src={anisocial} />
                                 <h3>Weeb Hub</h3>
                                 <p class="text-secondary">A social media website for anime lovers it has features that is unique from other sites.</p>
@@ -20,7 +20,7 @@ const OngoingProjects = () => {
                         </div>
 
                         <div class="col-md-6 col-lg-4">
-                            <div class="card-box text-center p-5">
+                            <div class="card-box text-center p-5 border shadow">
                                 <img src={bikeshop} width='66' height='66' />
                                 <h3 className='mt-4'>BikeShop</h3>
                                 <p class="text-secondary">An e-commerce bikeshop that has functionality an e-commerce website, login, cart, etc.</p>
@@ -30,7 +30,7 @@ const OngoingProjects = () => {
                         </div>
 
                         <div class="col-md-6 col-lg-4">
-                            <div class="card-box text-center p-5">
+                            <div class="card-box text-center p-5 border shadow">
                                 <img src={youtubed} width='66' height='66' />
                                 <h3 className='mt-4'>Youtube Downloader</h3>
                                 <p class="text-secondary">A youtbe downloader web app built with youtube downloader api.</p>
