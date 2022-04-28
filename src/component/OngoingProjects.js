2e75b0d521947a2b4ea1f74fb40ff9b07b2b27b3
unknown <raffyamoguis@gmail.com>
2022-04-28 11:36:28 +0800

    fix projects cards

--- src/component/OngoingProjects.js
+++ src/component/OngoingProjects.js
@@ -11,7 +11,7 @@ const OngoingProjects = () => {
                     <div class="row">
                         <div class="col-md-6 col-lg-4">
                             <div class="card-box text-center p-5 border shadow">
-                                <img src={anisocial} />
+                                <img src={anisocial} alt='Ongoing Projects' />
                                 <h3>Weeb Hub</h3>
                                 <p class="text-secondary">A social media website for anime lovers it has features that is unique from other sites.</p>
                                 <h6 className="card-subtitle mb-2 text-muted"><i className="bi bi-gear it-5"></i>In Development</h6>
@@ -21,7 +21,7 @@ const OngoingProjects = () => {
 
                         <div class="col-md-6 col-lg-4">
                             <div class="card-box text-center p-5 border shadow">
-                                <img src={bikeshop} width='66' height='66' />
+                                <img src={bikeshop} width='66' height='66' alt='Ongoing Projects' />
                                 <h3 className='mt-4'>BikeShop</h3>
                                 <p class="text-secondary">An e-commerce bikeshop that has functionality an e-commerce website, login, cart, etc.</p>
                                 <h6 className="card-subtitle mb-2 text-muted"><i className="bi bi-gear it-5"></i>In Development</h6>
@@ -31,7 +31,7 @@ const OngoingProjects = () => {
 
                         <div class="col-md-6 col-lg-4">
                             <div class="card-box text-center p-5 border shadow">
-                                <img src={youtubed} width='66' height='66' />
+                                <img src={youtubed} width='66' height='66' alt='Ongoing Projects' />
                                 <h3 className='mt-4'>Youtube Downloader</h3>
                                 <p class="text-secondary">A youtbe downloader web app built with youtube downloader api.</p>
                                 <h6 className="card-subtitle mb-2 text-muted"><i className="bi bi-gear it-5"></i>In Development</h6>
